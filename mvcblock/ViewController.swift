eb871de58e3d6ebcee6e94c32989031b7aeec54f
liuzilong <liuzilong@uboxol.com>
2017-04-27 14:56:12 +0800

    add NSURl

--- mvcblock/ViewController.swift
+++ mvcblock/ViewController.swift
@@ -42,9 +42,10 @@ class ViewController: UIViewController {
     
     func login() {
         getUserInfo(acc: "admin", pas: "admin") { (resp) in
-            if let result = resp as? Dictionary<String,AnyObject> {
-                print(result)
-            } else if let msg = resp as? String{
+//            if let result = resp as? Dictionary<String,AnyObject> {
+//                print(result)
+//            } else 
+            if let msg = resp as? String{
                 print(msg)
             } else {
                 print("发生了神秘事件！!")
@@ -62,14 +63,15 @@ class ViewController: UIViewController {
             DispatchQueue.global(qos: .default).async {
                 let dat = try? Data(contentsOf: url)
                 if let data = dat {
-                    let json = try? JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.mutableContainers)
-                    if let j = json {
-                        result(j)
-                    } else {
-                        result("JSON解析失败！")
-                    }
+//                    let json = try? JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.mutableContainers)
+//                    if let j = json {
+//                        result(j)
+//                    } else {
+//                        result("JSON解析失败！")
+//                    }
                     if let str = String(data: data, encoding: .utf8) {
                         result(str)
+                        print(result)
                     } else {
                         result("UTF8解码失败！")
                     }
@@ -82,6 +84,24 @@ class ViewController: UIViewController {
         }
     }
     
+    func GetRequest()
+    {
+        //(1）设置请求路径
+        let urlStr:NSString = String(format:"http://adongdev.top/php/route.php?zxd_path_name=login&account=%@&password=%@",userView.userNameF.text!,userView.passwordF.text!) as NSString
+        let url:NSURL = NSURL(string: urlStr as String)!
+        
+        //(2) 创建请求对象
+        let request:NSURLRequest = NSURLRequest(url: url as URL)
+        
+        //(3) 发送请求
+        NSURLConnection.sendAsynchronousRequest(request as URLRequest, queue:OperationQueue()) { (res, data, error)in
+            //服务器返回：请求方式 = GET，返回数据格式 = JSON，用户名 = 123，密码 = 1233
+            let  str = NSString(data: data!, encoding:String.Encoding.utf8.rawValue)
+            print(str)
+            
+        }
+    }
+    
     
     func abc(txt:String,pwd:String){
         print(txt)
@@ -91,7 +111,7 @@ class ViewController: UIViewController {
     
     func ed(){
         print("跳转")
-        login()
+        GetRequest()
 //        let signVc = SignViewController()
         //界面跳转，需现在sb中新建navigation，并箭头指向它
 //        self.navigationController?.pushViewController(signVc, animated: true)
